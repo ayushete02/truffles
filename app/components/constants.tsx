8c946b7a69214bebfb326bce807ed31e47bd902d
Ayush Shete <ayushete02@gmail.com>
2023-06-14 23:07:49 +0530

    Uodate asset dataset

--- app/components/constants.tsx
+++ app/components/constants.tsx
@@ -1,100 +1,74 @@
 export const AssetData = [
-      {
-        id: "1",
-        asset:"Bitcoin",
-        amount:"2346",
-        status:"sent",
-      },
-      {
-        id: "2",
-        asset:"Ethereum",
-        amount:"2346",
-        status:"Accepted",
-      },
-      {
-        id: "3",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "4",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "5",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "6",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "7",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "8",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "9",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "10",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "11",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "12",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "13",
-        asset:"",
-        amount:"",
-        status:"sent",
-      },
-      {
-        id: "14",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "15",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-      {
-        id: "16",
-        asset:"",
-        amount:"",
-        status:"",
-      },
-  
-     
-    ]
+  {
+    id: "1",
+    asset: "Bitcoin",
+    amount: "2346",
+    status: "Accepted",
+  },
+  {
+    id: "2",
+    asset: "Ethereum",
+    amount: "2346",
+    status: "sent",
+  },
+  {
+    id: "3",
+    asset: "Asset3",
+    amount: "Amount3",
+    status: "Accepted",
+  },
+  {
+    id: "4",
+    asset: "Asset4",
+    amount: "Amount4",
+    status: "sent",
+  },
+  {
+    id: "5",
+    asset: "Asset5",
+    amount: "Amount5",
+    status: "Accepted",
+  },
+  {
+    id: "6",
+    asset: "Asset6",
+    amount: "Amount6",
+    status: "sent",
+  },
+  {
+    id: "7",
+    asset: "Asset7",
+    amount: "Amount7",
+    status: "Accepted",
+  },
+  {
+    id: "8",
+    asset: "Asset8",
+    amount: "Amount8",
+    status: "sent",
+  },
+  {
+    id: "9",
+    asset: "Asset9",
+    amount: "Amount9",
+    status: "Accepted",
+  },
+  {
+    id: "10",
+    asset: "Asset10",
+    amount: "Amount10",
+    status: "sent",
+  },
+  {
+    id: "11",
+    asset: "Asset11",
+    amount: "Amount11",
+    status: "Accepted",
+  },
+  {
+    id: "12",
+    asset: "Asset12",
+    amount: "Amount12",
+    status: "sent",
+  },
+];
